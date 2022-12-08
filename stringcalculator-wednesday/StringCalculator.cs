8b68143e067866c01efbf2ce1b80c304dff6e615
Mark_Beebe <beebemjb@gmail.com>
2022-12-08 12:08:51 -0500

    brought in jeff's string calculator

---
+++ stringcalculator-wednesday/StringCalculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        var delimeters = new List<char> { ',', '\n' };
+        if (numbers.StartsWith("//"))
+        {
+            // "1*3"
+            delimeters.Add(numbers[2]);
+            numbers = numbers.Substring(4);
+        }
+        return numbers == "" ? 0 :numbers.Split(delimeters.ToArray()).Select(int.Parse).Sum();
+
+    }
+}
